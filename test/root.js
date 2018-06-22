95ffd2c69e29c4243d2a70343f204738c380c10f
saadnaveed <saadnvd2017@gmail.com>
2018-06-22 00:11:37 +0000

    automatically backed up by learn

--- test/root.js
+++ test/root.js
@@ -1,27 +1,15 @@
 global.expect = require('expect');
 
 const babel = require('babel-core');
-const jsdom = require('jsdom');
+const jsdom = require('mocha-jsdom');
 const path = require('path');
 
-before(function(done) {
-  const js = path.resolve(__dirname, '..', 'index.js');
-  const babelResult = babel.transformFileSync(js, {
+const babelResult = babel.transformFileSync(
+  path.resolve(__dirname, '..', 'index.js'), {
     presets: ['es2015']
-  });
+  }
+);
 
-  const html = path.resolve(__dirname, '..', 'index.html');
-
-  jsdom.env(html, [], {
-    src: babelResult.code,
-    virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-  }, (err, window) => {
-    if (err) {
-      return done(err);
-    }
-
-    global.window = window;
-
-    return done();
-  });
+jsdom({
+  src: babelResult.code
 });
